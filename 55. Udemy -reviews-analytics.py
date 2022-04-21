052e81d7a740788cc1b2b72cfbb66259b87014b7
christyfeifei <102733452+christyfeifei@users.noreply.github.com>
2022-04-21 13:56:38 +0800

    calculate average reviews.

--- 55. Udemy -reviews-analytics.py
+++ 55. Udemy -reviews-analytics.py
@@ -8,3 +8,9 @@ with open('reviews.txt', 'r') as f:
             print(len(data))  # 每讀一筆留言，印出數字
 
 print('檔案讀取完了, 總共有', len(data), '筆資料')  # 印出總共幾筆留言
+
+sum_len = 0
+for d in data:
+    sum_len = sum_len + len(d)
+
+print('留言平均長度為', sum_len/len(data))
